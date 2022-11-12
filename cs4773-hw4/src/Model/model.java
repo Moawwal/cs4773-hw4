ab46ac24f10fc34920a8d2a611dce69aa7b61d2f
Eduardo-Rosales-Tx <80177627+Eduardo-Rosales-Tx@users.noreply.github.com>
2022-11-12 12:35:21 -0600

    Add files via upload

---
+++ cs4773-hw4/src/Model/model.java
@@ -0,0 +1,67 @@
+package Model;
+
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+import application.view;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+public class model implements Initializable{
+	@FXML
+	private ListView<String> myListView;
+	@FXML
+	private TextField myName;
+	@FXML
+	private TextField myScore;
+	@FXML
+	private TextField myDate;
+	@FXML
+	private Button mySave;
+	
+	private String[] defaultContent = {"No name provided 0","No name provided 0","No name provided 0", 
+			"No name provided 0","No name provided 0"};
+	private ObservableList<String> content = FXCollections.observableArrayList(defaultContent);
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+		myListView.setItems(content);
+			
+		myListView.setOnMouseClicked(event -> {
+			int index = myListView.getSelectionModel().getSelectedIndex();
+			view newview = new view();
+			try {
+				newview.openGUI();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		
+	}
+
+	/*public ListView<String> getMyListView() {
+		return myListView;
+	}
+
+	public void setMyListView(ListView<String> myListView) {
+		this.myListView = myListView;
+	}*/
+	
+	
+}
---
+++ cs4773-hw4/src/ViewModel/viewModel.java
@@ -0,0 +1,15 @@
+package ViewModel;
+
+import java.util.ArrayList;
+
+import Model.model;
+
+public class viewModel {
+
+	public void updateListView(String newtext,int index) {
+		model mo = new model();
+		
+		
+	}
+	
+}
